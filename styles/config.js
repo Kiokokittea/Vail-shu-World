4baa3f9ef5dc355b4d2eedf448707e16cab8b951
Kiokokittea <167816773+Kiokokittea@users.noreply.github.com>
2024-04-22 14:00:15 -0700

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1JHFc4L2rQObZ4diWv3PStp5s8dSGEGiYOdjwunGZYwM";
 
 
 /* ------------------------------------------------------------------- */
